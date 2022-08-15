6d2da919c3cb8afa0553f8cd4c03bcacbc9a8c64
Victor Martinez Jr <victormtzcodes@gmail.com>
2022-08-15 16:02:36 -0400

    added fade in animation on page load

--- app.js
+++ app.js
@@ -1,1 +1,13 @@
 "use strict";
+
+const navContent = document.querySelector(".nav_content");
+const sectionTitle = document.querySelector(".section_title--model_3");
+const sectionP = document.querySelector(".section_p");
+const sectionBtns = document.querySelector(".section_btns");
+
+window.addEventListener("load", () => {
+  navContent.classList.add("fade-in");
+  sectionTitle.classList.add("fade-in");
+  sectionP.classList.add("fade-in");
+  sectionBtns.classList.add("fade-in");
+});
