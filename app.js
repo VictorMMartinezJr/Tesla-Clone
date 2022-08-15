4fecfdbf3238cce02c5787a8aec0dae223d045f5
Victor Martinez Jr <victormtzcodes@gmail.com>
2022-08-15 17:48:59 -0400

    added hidden nav on mobile

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 "use strict";
 
 const navContent = document.querySelector(".nav_content");
+const btnMenu = document.querySelector(".btn_menu");
+const btnClose = document.querySelector(".btn_close");
+const navLinks = document.querySelector(".nav_links");
 const sectionTitle = document.querySelector(".section_title--model_3");
 const fadeInElements = document.querySelectorAll(".fade-in");
 const sectionP = document.querySelector(".section_p--model-3");
@@ -12,7 +15,14 @@ window.addEventListener("load", () => {
   sectionP.classList.add("fade");
   sectionBtns.classList.add("fade");
 });
-console.log(fadeInElements);
+
+btnMenu.addEventListener("click", () => {
+  navLinks.classList.toggle("active");
+});
+btnClose.addEventListener("click", () => {
+  navLinks.classList.remove("active");
+});
+
 ///////////////////////////
 // INTERSECTION OBSERVER //
 ///////////////////////////
