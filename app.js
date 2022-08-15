03bfb9186554760ebc89ce6cf5ff5ba595d33816
Victor Martinez Jr <victormtzcodes@gmail.com>
2022-08-15 17:04:46 -0400

    added fade in animation on page scroll to section text elements

--- app.js
+++ app.js
@@ -2,12 +2,31 @@
 
 const navContent = document.querySelector(".nav_content");
 const sectionTitle = document.querySelector(".section_title--model_3");
-const sectionP = document.querySelector(".section_p");
-const sectionBtns = document.querySelector(".section_btns");
+const fadeInElements = document.querySelectorAll(".fade-in");
+const sectionP = document.querySelector(".section_p--model-3");
+const sectionBtns = document.querySelector(".section_btns--model-3");
 
 window.addEventListener("load", () => {
-  navContent.classList.add("fade-in");
-  sectionTitle.classList.add("fade-in");
-  sectionP.classList.add("fade-in");
-  sectionBtns.classList.add("fade-in");
+  navContent.classList.add("fade");
+  sectionTitle.classList.add("fade");
+  sectionP.classList.add("fade");
+  sectionBtns.classList.add("fade");
+});
+console.log(fadeInElements);
+///////////////////////////
+// INTERSECTION OBSERVER //
+///////////////////////////
+const fadeUpObserver = new IntersectionObserver((entries) => {
+  entries.forEach((entry) => {
+    if (entry.isIntersecting) {
+      entry.target.style.opacity = 1;
+    }
+    if (!entry.isIntersecting) {
+      entry.target.style.opacity = 0;
+    }
+  });
+}, {});
+
+fadeInElements.forEach((el) => {
+  fadeUpObserver.observe(el);
 });
